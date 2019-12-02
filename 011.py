68a51eda189b5747f8b82e554e765d0be609c2aa
lmm <meng@mys4s.cn>
2019-12-02 11:38:28 +0800

    11

---
+++ 011.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        mx  = 0
+        l = 0
+        r = len(height)
+        while l < r :
+            mx = max(mx , (r-l) * min(height[l],height[r]))
+            if height[l] < height[r]:
+                l +=1
+            else :
+                r -= 1
+        return mx
